312c4b9a1e58267e350ed85f4f91b34aa4cf43fc
Felipe Orlando <bee.sql@gmail.com>
2017-06-28 14:42:55 -0300

    Change project schema

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,36 @@
-// gulp and gulp-deps
-const gulp        = require("gulp");
-const sass        = require("gulp-sass");
-const cleanCSS    = require("gulp-clean-css");
-const imagemin    = require("gulp-imagemin");
-const livereload  = require("gulp-livereload");
-const browserSync = require("browser-sync").create();
+const gulp        = require('gulp');
+const sass        = require('gulp-sass');
+const livereload  = require('gulp-livereload');
+const browserSync = require('browser-sync').create();
 
-// sass to css
-gulp.task("sass", () => {
-  return gulp.src("./source/style/main.sass")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(cleanCSS({compatibility: "ie8"}))
-    .pipe(gulp.dest("./dist/style/"));
+gulp.task('sass', () => {
+  return gulp.src('./source/style/main.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/style/'));
 });
 
-// compress images
-gulp.task("images", () => {
-  return gulp.src("./source/images/**/*")
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}]
-    }))
-    .pipe(gulp.dest("./dist/images/"));
+gulp.task('views', () => {
+  return gulp.src('./source/views/**/*')
+    .pipe(gulp.dest('./dist/views'));
 });
 
-// watch diffs
-gulp.task("watch", () => {
+gulp.task('watch', () => {
   livereload.listen();
-  gulp.watch("./source/style/**/*",  ["sass"]);
-  gulp.watch("./source/images/**/*", ["images"]);
+  gulp.watch('./source/style/**/*', ['sass']);
+  gulp.watch('./source/views/**/*', ['views']);
 });
 
-// server with browserSync
-gulp.task("serve", ["sass"], () => {
+gulp.task('serve', ['sass'], () => {
   browserSync.init({
     port: 8000,
     server: {
-      baseDir: "./dist/"
+      baseDir: './dist/'
     }
   });
 
-  gulp.watch("./dist/**/*").on("change", browserSync.reload);
+  gulp.watch('./dist/**/*').on('change', browserSync.reload);
 });
 
-// default task
-gulp.task("default", [
-  "sass", "images", "serve", "watch"
+gulp.task('default', [
+  'sass', 'views', 'serve', 'watch'
 ]);
