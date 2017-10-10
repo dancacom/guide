6cffb051f8f7a4f44bcda8599f4ebac106ed9337
Felipe Orlando <bee.sql@gmail.com>
2017-10-10 11:52:40 -0300

    Fix menu overlay

--- source/javascripts/components/menu.js
+++ source/javascripts/components/menu.js
@@ -1,7 +1,19 @@
 (function() {
   var $menu = $('#menu-dropdown');
+  var $overlay = $('.overlay');
+  var $body = $('body');
+
+  var triggerEvt = function() {
+    $menu.toggleClass('menu--active');
+    $overlay.fadeToggle(200);
+    $body.toggleClass('body--no-scroll');
+  };
 
   $menu.on('click', '#open', function() {
-    $menu.toggleClass('menu--active');
+    triggerEvt();
+  });
+
+  $overlay.on('click', function() {
+    triggerEvt();
   });
 })();
